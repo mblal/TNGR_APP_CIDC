d8b40412bcc457fec4dc0b2f615cfa4e1d4caa8e
mblal <mohammed.blal@gmail.com>
2023-01-01 14:28:59 +0100

    Update env.sh

--- conf/TNGR_APP/env.sh
+++ conf/TNGR_APP/env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export SOCLE="${SOCLE:-nodejs}"
 export DOCKER_IMAGE="${DOCKER_IMAGE:-mblcontainerize/nodejs:latest}"
 export GIT_SOURCE=git@github.com:mblal/TNGR_APP.git
 export GIT_SOURCE_BRANCH=develop
